296b26377640060163eee1098f416061a885f15c
Sebastian <mateo.lomas@yachaytech.edu.ec>
2023-05-29 19:09:03 -0500

    Set ios app icon

--- ios/Elucidate/AppDelegate.mm
+++ ios/Elucidate/AppDelegate.mm
@@ -7,7 +7,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"Intelligentia";
+  self.moduleName = @"Elucidate";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
--- ios/ElucidateTests/ElucidateTests.m
+++ ios/ElucidateTests/ElucidateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface IntelligentiaTests : XCTestCase
+@interface ElucidateTests : XCTestCase
 
 @end
 
-@implementation IntelligentiaTests
+@implementation ElucidateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
